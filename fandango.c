e2728ed90a70019b8fa47e1896623e1bc016eef5
Michael Cullen <michael181189@gmail.com>
2012-05-19 15:57:05 +0100

    Made it map memory correctly.

--- fandango.c
+++ fandango.c
@@ -10,6 +10,7 @@
 #include <linux/mm.h> // for si_meminfo
 #include <asm/page.h> //for PAGE_SHIFT
 #include <linux/random.h> //for get_random_bytes
+#include <asm/io.h> //for pointer translations
 
 long frandom() {
       long val;
@@ -26,10 +27,13 @@ void* random_pointer() {
 }
 
 void fandango() {
+	int num = 5;
 	void* ptr = random_pointer();
 	int data = (int)frandom();
-	printk("Dancing on pointer %p with the number %d\n",ptr,data);
-	*((int*)ptr) = data; //the business end of the fandango!
+	printk("Example pointers are %p, %p, and %p\n",&data,&ptr,&num);
+	printk("Page offset is %p and data is at %p physically \n",PAGE_OFFSET,virt_to_phys(&data));
+	printk("(would) Dancing on pointer %p (virtually %p) with the number %d\n",ptr,phys_to_virt(ptr),data);
+	*((int*)phys_to_virt(ptr)) = data; //the business end of the fandango!
 }
 
 int init()
@@ -39,7 +43,7 @@ int init()
 	si_meminfo(&si);
 	printk("Fandango module reporting for duty and ready to DANCE!\n");
 	printk("Or not as it's not written yet. Though there is %ld bytes of memory here!\n",si.totalram << PAGE_SHIFT);
-	for(i=0;i<10000;i++)
+	//for(i=0;i<10000;i++)
 	  fandango();
 	return 0;
 }
